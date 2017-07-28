69a558523912bf49ceed5c5a7530f9275cffc6eb
Wm Costantini <costantiniwm@upmc.edu>
2017-07-28 17:36:44 -0400

    Get Tasks

--- ApiTut.py
+++ ApiTut.py
@@ -2,8 +2,4 @@
 
 import requests
 
-resp = requests.get('https://todolist.example.com/tasks/')
-if resp.status_code != 200:
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
+
